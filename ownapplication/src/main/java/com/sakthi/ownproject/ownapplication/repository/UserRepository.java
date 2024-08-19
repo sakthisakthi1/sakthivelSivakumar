cbf731842b4fd926a87d0b2d6818e94d463f1ad7
Sakthivel Sivakumar <sivasakthivel20000@gmail.com>
2024-08-19 13:59:51 +0530

    Added Dependecy for Tomcat in pom.xml and commented repo for intial DB conection

--- ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/repository/UserRepository.java
+++ ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/repository/UserRepository.java
@@ -1,6 +1,16 @@
 package com.sakthi.ownproject.ownapplication.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sakthi.ownproject.ownapplication.entity.User;
+
 // @Repository
 // public interface UserRepository extends JpaRepository<Long, User> {
 
+
+  
 // }
