ec103c2a3ee8ececa46b5856dec4de6c5e4f5617
Sakthivel Sivakumar <sivasakthivel20000@gmail.com>
2024-08-19 18:14:13 +0530

    Added with Javax Persistance Depedency and JPA Dependency with Entity and Database Configuration with repository Implementation

--- ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/entity/User.java
+++ ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/entity/User.java
@@ -2,42 +2,55 @@ package com.sakthi.ownproject.ownapplication.entity;
 
 import java.math.BigInteger;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_config_detail", schema = "\"secured\"")
 public class User {
 
-    private BigInteger userID;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger userId;
 
-    private String userEmailId;
+    @Column(name = "user_email")
+    private String userEmail;
 
+    @Column(name = "user_credential")
     private String userCredentials;
 
-    public BigInteger getUserID() {
-        return userID;
+    public BigInteger getUserId() {
+        return userId;
     }
 
-    public String getUserEmailId() {
-        return userEmailId;
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
     public String getUserCredentials() {
         return userCredentials;
     }
 
-    public void setUserID(BigInteger userID) {
-        this.userID = userID;
-    }
-
-    public void setUserEmailId(String userEmailId) {
-        this.userEmailId = userEmailId;
-    }
-
     public void setUserCredentials(String userCredentials) {
         this.userCredentials = userCredentials;
     }
 
-    public User(BigInteger userID, String userEmailId, String userCredentials) {
-        this.userID = userID;
-        this.userEmailId = userEmailId;
-        this.userCredentials = userCredentials;
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", userEmail=" + userEmail + ", userCredentials=" + userCredentials + "]";
     }
 
 }
--- ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/repository/UserRepository.java
+++ ownapplication/src/main/java/com/sakthi/ownproject/ownapplication/repository/UserRepository.java
@@ -1,16 +1,11 @@
 package com.sakthi.ownproject.ownapplication.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sakthi.ownproject.ownapplication.entity.User;
 
-// @Repository
-// public interface UserRepository extends JpaRepository<Long, User> {
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
 
-
-  
-// }
+}
